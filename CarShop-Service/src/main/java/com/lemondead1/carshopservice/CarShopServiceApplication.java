41133dfda6f073005ace4dc2bf2c7235d493621f
Olexy <lemondead1@gmail.com>
2024-08-15 01:51:28 +0300

    Setup Jetty.

--- CarShop-Service/src/main/java/com/lemondead1/carshopservice/CarShopServiceApplication.java
+++ CarShop-Service/src/main/java/com/lemondead1/carshopservice/CarShopServiceApplication.java
@@ -1,57 +1,76 @@
 package com.lemondead1.carshopservice;
 
-import com.lemondead1.carshopservice.cli.CLI;
-import com.lemondead1.carshopservice.cli.CommandAcceptor;
-import com.lemondead1.carshopservice.cli.command.builders.CommandRootBuilder;
-import com.lemondead1.carshopservice.controller.*;
 import com.lemondead1.carshopservice.database.DBManager;
 import com.lemondead1.carshopservice.repo.CarRepo;
 import com.lemondead1.carshopservice.repo.EventRepo;
 import com.lemondead1.carshopservice.repo.OrderRepo;
 import com.lemondead1.carshopservice.repo.UserRepo;
 import com.lemondead1.carshopservice.service.*;
+import com.lemondead1.carshopservice.servlet.HelloWorld;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jetty.ee10.servlet.ServletHandler;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.ee10.webapp.WebAppContext;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.ResourceFactory;
 
 import java.io.IOException;
 import java.util.Properties;
 
+@Slf4j
 public class CarShopServiceApplication {
   private static boolean exited = false;
 
-  public static void main(String[] args) throws IOException {
-    var dbManager = createDBManagerWithConfigFile();
-    dbManager.setupDatabase();
+  public static void main(String[] args) throws Exception {
+    var configs = readConfigs();
 
-    var userRepo = new UserRepo(dbManager);
-    var carRepo = new CarRepo(dbManager);
-    var orderRepo = new OrderRepo(dbManager);
-    var eventRepo = new EventRepo(dbManager);
+//    var dbManager = createDBManagerWithConfigs(configs);
+//    dbManager.setupDatabase();
+//
+//
+//    var userRepo = new UserRepo(dbManager);
+//    var carRepo = new CarRepo(dbManager);
+//    var orderRepo = new OrderRepo(dbManager);
+//    var eventRepo = new EventRepo(dbManager);
+//
+//    var timeService = new TimeService();
+//    var eventService = new EventService(eventRepo, timeService);
+//    var userService = new UserService(userRepo, orderRepo, eventService);
+//    var orderService = new OrderService(orderRepo, carRepo, eventService, timeService);
+//    var carService = new CarService(carRepo, orderRepo, eventService);
+//    var sessionService = new SessionService(userRepo, eventService);
 
-    var timeService = new TimeService();
-    var eventService = new EventService(eventRepo, timeService);
-    var userService = new UserService(userRepo, orderRepo, eventService);
-    var orderService = new OrderService(orderRepo, carRepo, eventService, timeService);
-    var carService = new CarService(carRepo, orderRepo, eventService);
-    var sessionService = new SessionService(userRepo, eventService);
-
-    var cli = CLI.getBestAvailable();
-    var commandBuilder = new CommandRootBuilder();
-    new LoginController(sessionService).registerEndpoints(commandBuilder);
-    new HomeController(() -> exited = true).registerEndpoints(commandBuilder);
-    new CarController(carService).registerEndpoints(commandBuilder);
-    new OrderController(orderService, carService, userService).registerEndpoints(commandBuilder);
-    new UserController(userService).registerEndpoints(commandBuilder);
-    new EventController(eventService).registerEndpoints(commandBuilder);
-
-    var rootCommand = commandBuilder.build();
-
-    new CommandAcceptor(() -> !exited, cli, sessionService, rootCommand, dbManager).acceptCommands();
+    var server = setupJettyWithConfigs(configs);
+    server.start();
+    server.join();
   }
 
-  private static DBManager createDBManagerWithConfigFile() throws IOException {
+  private static Server setupJettyWithConfigs(Properties cfg) {
+    var port = Integer.parseInt(cfg.getProperty("port"));
+    var server = new Server(port);
+    var context = new WebAppContext();
+    var resourceFactory = ResourceFactory.of(context);
+    var baseResource = resourceFactory.newClassLoaderResource("webapp.xml");
+    context.setBaseResource(baseResource);
+    context.setContextPath("/");
+    context.setParentLoaderPriority(true);
+
+
+    context.addServlet(new HelloWorld(), "/hello");
+
+    server.setHandler(context);
+    return server;
+  }
+
+  private static Properties readConfigs() throws IOException {
     var cfg = new Properties();
     try (var reader = ClassLoader.getSystemResourceAsStream("config.properties")) {
       cfg.load(reader);
     }
+    return cfg;
+  }
+
+  private static DBManager createDBManagerWithConfigs(Properties cfg) throws IOException {
     return new DBManager(cfg.getProperty("jdbc_url"),
                          cfg.getProperty("database_user"),
                          cfg.getProperty("database_password"),
